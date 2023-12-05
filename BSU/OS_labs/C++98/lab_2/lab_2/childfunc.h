130e21a39f61ed34836fe18788775340e153d4c6
Ян <145141197+YanSokolovsky@users.noreply.github.com>
2023-12-05 21:47:15 +0300

    Delete BSU/OS_labs/C++98/lab_2/lab_2 directory

--- BSU/OS_labs/C++98/lab_2/lab_2/childfunc.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <windows.h>
-#include <conio.h>
-#include <iostream>
-#include <string>
-using namespace std;
-void sort(int* a, int first, int last);
-int* MakeMass(char* arg[], int count);
